e6d09421c0ab1fa083b7c8573371ec15567f71b1
moslim b <moslimmoslim196@gamil.com>
2024-08-12 10:45:34 +0200

    last update

---
+++ echo.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "moslimbargeoui@gmail.com"; // Replace with your email address
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    $mailBody = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to, $subject, $mailBody, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
